83bb0bce0b203014b2b6d9818f55724ad14dfa04
damazus <liginmlugudor@gmail.com>
2018-11-28 23:19:08 +0000

    header responsive

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,9 @@
-console.log('ok')
+document.addEventListener('DOMContentLoaded', function () {
+    const $header = document.querySelector('#header')
+    const $trigger = document.querySelector('#header__trigger')
+
+    $trigger.addEventListener('click', function () {
+        console.log($header)
+        $header.classList.toggle('header--mobile')
+    })
+})
