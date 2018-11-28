cbe5563e76ac46199bbcd2af80145bde62e453cc
damazus <liginmlugudor@gmail.com>
2018-11-28 23:41:55 +0000

    header close trigger

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     const $header = document.querySelector('#header')
+    const $brand = document.querySelector('#header__brand')
     const $trigger = document.querySelector('#header__trigger')
+    const $menu_links = document.querySelectorAll('.header-menu-link')
+    const active_classname = 'header--mobile'
 
-    $trigger.addEventListener('click', function () {
-        console.log($header)
-        $header.classList.toggle('header--mobile')
+    $trigger.addEventListener('click', function (e) {
+        e.preventDefault()
+        $header.classList.toggle(active_classname)
+    })
+
+    const closeMenu = function (e) {
+        e.preventDefault()
+        $header.classList.contains(active_classname) ? $header.classList.remove(active_classname) : null
+    }
+
+    $brand.addEventListener('click', closeMenu)
+
+    $menu_links.forEach(($item) => {
+        $item.addEventListener('click', closeMenu)
     })
 })
